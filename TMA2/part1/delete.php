70f82a8b8b36999cef40b02e948534c5b45e10c1
Muhammad Sajjad Salman <mssalman@uwaterloo.ca>
2023-09-23 03:47:00 -0400

    Delete TMA2 directory

--- TMA2/part1/delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    include 'config.php';
-    $id = $_GET['ID'];
-    $sql = "DELETE FROM `bookmark` WHERE id='$id'";
-    mysqli_query($con, $sql);
-    header('location: home.php');
-?>
--- TMA2/part1/table.js
+++
@@ -1,9 +0,0 @@
-function showTable(){
-    var table = document.getElementById("tableVisibility");
-    var rowCount = table.rows.length;
-    if (rowCount > 1) {
-        document.getElementById('tableVisibility').hidden = false
-    } else {
-        document.getElementById('tableVisibility').hidden = true
-    }
-}
